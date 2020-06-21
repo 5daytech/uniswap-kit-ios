8b35a7817e0eff8c6c52367e902099acdc7773aa
Abai Abakirov <abaikirov@gmail.com>
2020-06-21 15:54:45 +0600

    projects

---
+++ UniswapKit/UniswapKit/UniswapKit.h
@@ -0,0 +1,19 @@
+//
+//  UniswapKit.h
+//  UniswapKit
+//
+//  Created by Abai Abakirov on 6/21/20.
+//  Copyright © 2020 FridayTech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UniswapKit.
+FOUNDATION_EXPORT double UniswapKitVersionNumber;
+
+//! Project version string for UniswapKit.
+FOUNDATION_EXPORT const unsigned char UniswapKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UniswapKit/PublicHeader.h>
+
+
---
+++ UniswapKitDemo/UniswapKitDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UniswapKitDemo
+//
+//  Created by Abai Abakirov on 6/21/20.
+//  Copyright © 2020 FridayTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
